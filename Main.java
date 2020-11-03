2884bacc2907ac987e4810f6dab4464c0bf218b6
hscragg <hayden.scragg@patelhs.org>
2020-11-03 15:47:03 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[] value = { 3, 4, 6, 345, 999, 333, 32, 420, 21, 1933, 1738 };
+
+    for (int i = 10; i >= 0; i--) {
+
+      if (value[i] % 2 == 0) {
+
+        System.out.println(value[i]);
+      }
+    }
+
   }
 }
